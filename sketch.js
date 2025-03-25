531bf09d250c35b8950c791912f3de6e769f8489
clod10 <claudia.angileri@gmail.com>
2025-03-25 12:59:48 +0100

    aggiunto nome

--- sketch.js
+++ sketch.js
@@ -10,6 +10,10 @@ function draw() {
   rect(0, height - s, s, s);
   rect(width - s, 0, s, s);
   rect(width - s, height - s, s, s);
+
+  textSize(72);
+  textAlign("center");
+  text("Claudia", width / 2, height / 2);
 }
 
 function windowResized() {
